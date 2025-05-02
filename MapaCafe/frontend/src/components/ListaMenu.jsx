b3b58525928c5403b971f98cc6ee21e736487ca3
ErikArsego <erik.arsego@outlook.com>
2025-05-02 12:17:09 -0300

    Menu Lateral
    Criado menu lateral

---
+++ MapaCafe/frontend/src/components/ListaMenu.jsx
@@ -0,0 +1,20 @@
+import { Menu } from "antd";
+import { Link } from "react-router-dom";
+
+const ListaMenu = () => {
+
+    const items = [
+        {
+            key: 'btnInicio',
+            label: 'Início'
+        },
+        {
+            key: 'btnMinhasCafeterias',
+            label: 'Minhas Cafeterias'
+        },
+        {
+            key: 'btnMeuPerfil',
+            label: 'Meu Perfil'
+        }
+    ]   
+}
