d7d8b49372d4c2e0a5c7cd9cea837a75e52d9bdf
carolgraser <carolina_graser@outlook.com>
2025-05-07 18:51:43 -0300

    Alterando a classe CadastroCafeteria para adicionar novos atributos

--- MapaCafe/Models/CadastroCafeteria.cs
+++ MapaCafe/Models/CadastroCafeteria.cs
@@ -2,7 +2,11 @@ _namespace MapaCafe.Models
 public class CadastroCafeteria{
     public int id{ get; set; };
     public string nomeCafeteria{ get; set; } = string.Empty;
-    public string localizacaoCafeteria{ get; set; } = string.Empty;
+    public string ruaCafeteria{ get; set; } = string.Empty;
+    public string? complementoEndereco{ get; set; };
+    public string bairroCafeteria{get; set; } = string.Empty;
+    public int numeroEndereco{get; set; };
+    public string? cepEndereco{get; set;};
     public string comidasFavoritas{ get; set; } = string.Empty; 
     public int avaliacaoCafeteria{get; set; };
     public string observacoesCafeteria{get; set; } = string.Empty;
